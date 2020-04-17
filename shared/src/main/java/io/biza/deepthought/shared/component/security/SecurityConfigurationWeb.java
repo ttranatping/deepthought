ee0c8c6dc7d1eb7bd4eae3e7e8ddcbadc316d996
Stuart Low <Stuart.Low@me.com>
2020-04-17 15:54:22 +1000

    Fix PS256 support

--- shared/src/main/java/io/biza/deepthought/shared/component/security/SecurityConfigurationWeb.java
+++ shared/src/main/java/io/biza/deepthought/shared/component/security/SecurityConfigurationWeb.java
@@ -13,20 +13,29 @@
  *******************************************************************************/
 package io.biza.deepthought.shared.component.security;
 
+import java.security.Provider;
+import java.security.Security;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurationWeb extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
-    // JWT Validation
-    http.oauth2ResourceServer().jwt();
-    // Anonymous access, method security to pickup
-    // http.anonymous();
-  }
-}
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		/**
+		 * Inject RSASSA-PSS support into runtime
+		 */
+		Provider bc = BouncyCastleProviderSingleton.getInstance();
+		Security.addProvider(bc);
 
+		// JWT Validation
+		http.oauth2ResourceServer().jwt();
+		// Anonymous access, method security to pickup
+		// http.anonymous();
+	}
+}
